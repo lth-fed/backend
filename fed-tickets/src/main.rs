4fe1aeea0dfaac30f50b502d8bb9b094adad280a
Mostafa Kerim <25439218+confusinguser@users.noreply.github.com>
2026-04-23 21:28:30 +0200

    [DB] (Update): Add database migration support

--- fed-tickets/src/main.rs
+++ fed-tickets/src/main.rs
@@ -5,6 +5,8 @@ pub mod groups;
 use color_eyre::eyre::Context;
 use poem::{Route, Server, listener::TcpListener};
 use poem_openapi::OpenApiService;
+use sqlx::migrate::MigrateDatabase;
+use sqlx::Postgres;
 use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
@@ -12,11 +14,17 @@ async fn main() -> color_eyre::Result<()> {
     let _ = dotenvy::dotenv();
 
     let db_url = std::env::var("DATABASE_URL").wrap_err("DATABASE_URL not set")?;
+    if !Postgres::database_exists(&db_url).await? {
+        Postgres::create_database(&db_url).await?;
+    }
+
     let db = PgPoolOptions::new()
         .max_connections(50)
         .connect(&db_url)
         .await
         .wrap_err("failed to connect to database")?;
+
+    sqlx::migrate!("./migrations").run(&db).await?;
 
     let context = context::Context { db };
     let api_service = OpenApiService::new(
