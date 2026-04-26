c87b449f6ea58466ab56d369469f36c6ed167660
Icelk <main@icelk.dev>
2026-04-26 13:05:49 +0200

    [DB] (Update): Comply with GDPR in user data storage

--- schema/user.sql
+++ schema/user.sql
@@ -1,8 +1,12 @@
 create table users (
     -- `stil-id:<stil-id>` or `email:<email>`
     id text primary key,
-    name text not null,
-    language text not null,
+    -- needs to be pseudoanonymised according to GDPR, therefore we have to store the encrypted bytes
+    name bytea not null,
+    -- same as for name, also remember to salt so the encrypted data isn't the same for all users with the same language
+    language bytea not null,
+    -- GDPR remove all accounts older than 2 years after this: this is a GDPR requirement
+    latest_refresh timestamp not null,
     -- don't remove name when becoming inactive!
     is_active boolean not null
     -- constraint no_name_when_deleted check ((not is_active) and name = null or is_active and name != null)
