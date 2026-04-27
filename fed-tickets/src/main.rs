15c3aa042dd1e2a9293639c17f8d46967b9aaac8
Åke Amcoff <ake@amcoff.net>
2026-04-27 19:15:23 +0200

    [DB] (Update): Add SQLx boilerplate
    * wip
    
    * [DB] (Update): Add database migration support
    
    ---------
    
    Co-authored-by: Mostafa Kerim <25439218+confusinguser@users.noreply.github.com>

--- fed-tickets/src/main.rs
+++ fed-tickets/src/main.rs
@@ -1,16 +1,43 @@
 pub mod activities;
+pub mod context;
 pub mod groups;
 
+use color_eyre::eyre::Context;
 use poem::{Route, Server, listener::TcpListener};
 use poem_openapi::OpenApiService;
+use sqlx::migrate::MigrateDatabase;
+use sqlx::Postgres;
+use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
+    let _ = dotenvy::dotenv();
+
+    let db_url = std::env::var("DATABASE_URL").wrap_err("DATABASE_URL not set")?;
+    if !Postgres::database_exists(&db_url).await? {
+        Postgres::create_database(&db_url).await?;
+    }
+
+    let db = PgPoolOptions::new()
+        .max_connections(50)
+        .connect(&db_url)
+        .await
+        .wrap_err("failed to connect to database")?;
+
+    sqlx::migrate!("./migrations").run(&db).await?;
+
+    let context = context::Context { db };
     let api_service = OpenApiService::new(
-        (activities::Router, groups::Router),
+        (
+            activities::Router {
+                context: context.clone(),
+            },
+            groups::Router { context },
+        ),
         env!("CARGO_PKG_NAME"),
         env!("CARGO_PKG_VERSION"),
     )
+    // this url is just for the Swagger UI
     .server("http://localhost:8000/v0");
     let ui = api_service.swagger_ui();
 
