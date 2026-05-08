9c35b82d50522067dca94f4a27c974552317a4d6
Esbjörn Stenberg <me@stagrim.com>
2026-05-08 21:49:29 +0200

    [API] (Refactor): Fix clippy errors

--- fed-tickets/src/healthcheck.rs
+++ fed-tickets/src/healthcheck.rs
@@ -20,11 +20,11 @@ impl Router {
     #[oai(path = "/healthcheck", method = "get")]
     async fn health_check(&self) -> HealthCheck {
         match sqlx::query("SELECT 1").fetch_one(&self.context.db).await {
-            Ok(_) => HealthCheck::Ok(Json("Ok :)".to_string())),
-            Err(e) => {
+            Ok(_) => HealthCheck::Ok(Json("Ok :)".to_owned())),
+            Err(error_or_something_idk) => {
                 let timeout_secs = self.context.db.options().get_acquire_timeout().as_secs();
                 tracing::error!(
-                    "Health check database failure: '{e}' with timeout {timeout_secs}s"
+                    "Health check database failure: '{error_or_something_idk}' with timeout {timeout_secs}s"
                 );
                 HealthCheck::Err(Json(format!(
                     "Service Unavailable: Database connection failed with timeout set to {timeout_secs}s",
