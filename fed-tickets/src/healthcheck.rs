902c9160e244152f57ced31ffa2d538d030f7608
Esbjörn Stenberg <me@stagrim.com>
2026-05-08 21:42:58 +0200

    [API] (Update): Add health check route

---
+++ fed-tickets/src/healthcheck.rs
@@ -0,0 +1,35 @@
+use poem_openapi::{ApiResponse, OpenApi, payload::Json};
+
+use crate::context::Context;
+
+#[derive(Clone, Debug)]
+pub struct Router {
+    pub context: Context,
+}
+
+#[derive(Debug, ApiResponse)]
+pub enum HealthCheck {
+    #[oai(status = 200)]
+    Ok(Json<String>),
+    #[oai(status = 503)]
+    Err(Json<String>),
+}
+
+#[OpenApi]
+impl Router {
+    #[oai(path = "/healthcheck", method = "get")]
+    async fn health_check(&self) -> HealthCheck {
+        match sqlx::query("SELECT 1").fetch_one(&self.context.db).await {
+            Ok(_) => HealthCheck::Ok(Json("Ok :)".to_string())),
+            Err(e) => {
+                let timeout_secs = self.context.db.options().get_acquire_timeout().as_secs();
+                tracing::error!(
+                    "Health check database failure: '{e}' with timeout {timeout_secs}s"
+                );
+                HealthCheck::Err(Json(format!(
+                    "Service Unavailable: Database connection failed with timeout set to {timeout_secs}s",
+                )))
+            }
+        }
+    }
+}
