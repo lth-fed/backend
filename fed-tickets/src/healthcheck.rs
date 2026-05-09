347aacffd979a225f64dc441971a18b06d8b73d8
Icelk <main@icelk.dev>
2026-05-09 12:19:03 +0200

    [API] (Refactor): Use plaintext instead of json for healthcheck

--- fed-tickets/src/healthcheck.rs
+++ fed-tickets/src/healthcheck.rs
@@ -1,4 +1,4 @@
-use poem_openapi::{ApiResponse, OpenApi, payload::Json};
+use poem_openapi::{ApiResponse, OpenApi, payload::PlainText};
 
 use crate::context::Context;
 
@@ -10,9 +10,9 @@ pub struct Router {
 #[derive(Debug, ApiResponse)]
 pub enum HealthCheck {
     #[oai(status = 200)]
-    Ok(Json<String>),
+    Ok(PlainText<String>),
     #[oai(status = 503)]
-    Err(Json<String>),
+    Err(PlainText<String>),
 }
 
 #[OpenApi]
@@ -20,13 +20,13 @@ impl Router {
     #[oai(path = "/healthcheck", method = "get")]
     async fn health_check(&self) -> HealthCheck {
         match sqlx::query("SELECT 1").fetch_one(&self.context.db).await {
-            Ok(_) => HealthCheck::Ok(Json("Ok :)".to_owned())),
+            Ok(_) => HealthCheck::Ok(PlainText("Ok :)".to_owned())),
             Err(error_or_something_idk) => {
                 let timeout_secs = self.context.db.options().get_acquire_timeout().as_secs();
                 tracing::error!(
                     "Health check database failure: '{error_or_something_idk}' with timeout {timeout_secs}s"
                 );
-                HealthCheck::Err(Json(format!(
+                HealthCheck::Err(PlainText(format!(
                     "Service Unavailable: Database connection failed with timeout set to {timeout_secs}s",
                 )))
             }
