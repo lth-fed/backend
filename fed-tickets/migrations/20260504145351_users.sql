df9e0652a18a1e0e97e01cdc6c34100233076edb
Icelk <main@icelk.dev>
2026-05-14 17:29:41 +0200

    [DB] (Update): Add nonce for encryption

--- fed-tickets/migrations/20260504145351_users.sql
+++ fed-tickets/migrations/20260504145351_users.sql
@@ -1,12 +1,15 @@
 create table users (
-    -- `stil-id:<stil-id>` or `email:<email>`
+    -- `lund-university:<stil-id>` or `email:<email>`
     id text primary key,
     -- needs to be pseudoanonymised according to GDPR, therefore we have to store the encrypted bytes
     name bytea not null,
     -- same as for name, also remember to salt so the encrypted data isn't the same for all users with the same language
     language bytea not null,
+    -- used by encryption
+    nonce bytea not null,
     -- GDPR remove all accounts older than 2 years after this: this is a GDPR requirement
     latest_refresh timestamptz not null,
+    creation timestamptz not null,
     -- don't remove name when becoming inactive!
     inactive_since timestamptz
     -- constraint no_name_when_deleted check ((not is_active) and name = null or is_active and name != null)
