dcd8f28ec52655e62fa4d3dc30796f0d39f649f7
Icelk <main@icelk.dev>
2026-05-18 17:19:30 +0200

    [API] (Update): Add convenience methods for encryption & decryption

--- fed-tickets/src/context.rs
+++ fed-tickets/src/context.rs
@@ -1,5 +1,9 @@
 use std::path::PathBuf;
 
+use base64::Engine as _;
+use chacha20::ChaCha20;
+use chacha20::cipher::KeyIvInit as _;
+use chacha20::cipher::StreamCipher as _;
 use color_eyre::Section as _;
 use color_eyre::eyre::Context as _;
 use sqlx::migrate::MigrateDatabase as _;
@@ -7,10 +11,6 @@ use sqlx::postgres::PgPoolOptions;
 use sqlx::{PgPool, Postgres};
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::EnvFilter;
-use chacha20::ChaCha20;
-use chacha20::cipher::KeyIvInit as _;
-use chacha20::cipher::StreamCipher as _;
-use sqlx::PgPool;
 
 #[derive(Debug, Clone)]
 pub struct Context {
@@ -18,28 +18,6 @@ pub struct Context {
     encryption_key: [u8; 32],
 }
 
-impl Context {
-    #[must_use]
-    pub fn new(db: PgPool, encryption_key: [u8; 32]) -> Self {
-        Context { db, encryption_key }
-    }
-    /// Encrypts and decrypts any data using chacha20 given a 12 byte nonce.
-    ///
-    /// ## Example
-    /// ```rs
-    /// let nonce: [u8; 12] = rand::random();
-    /// let data = b"secret data";
-    /// let encrypted = context.endecrypt(data, &nonce);
-    /// let decrypted = context.endecrypt(&encrypted, &nonce);
-    /// ```
-    #[must_use]
-    pub fn endecrypt(&self, data: &[u8], nonce: &[u8; 12]) -> Vec<u8> {
-        let mut cipher = ChaCha20::new(&self.encryption_key.into(), nonce.into());
-        let mut buffer = data.to_owned();
-        cipher.apply_keystream(&mut buffer);
-        buffer
-    }
-}
 impl Context {
     /// # Errors
     ///
@@ -64,8 +42,16 @@ impl Context {
                 .wrap_err("Failed to set up the database")
                 .suggestion("Start the database with `docker compose up -d`")?
         };
+        let encryption_key = std::env::var("ENCRYPTION_KEY")
+            .wrap_err("Error: Missing env variable: 'ENCRYPTION_KEY'.")?;
+        let encryption_key = base64::engine::general_purpose::STANDARD
+            .decode(encryption_key)
+            .wrap_err("Error: Could not parse env variable 'ENCRYPTION_KEY' as base64.")?;
+        let encryption_key = <[u8; 32]>::try_from(encryption_key.as_slice()).wrap_err(
+            "Error: Env variable 'ENCRYPTION_KEY' is of wrong size. Expected: 32 bytes.",
+        )?;
 
-        let context = Self { db };
+        let context = Self { db, encryption_key };
         Ok(context)
     }
     async fn setup_db(db_url: &str) -> color_eyre::Result<PgPool> {
@@ -88,5 +74,75 @@ impl Context {
             .wrap_err("Failed to run migrations")?;
 
         Ok(db)
+    }
+
+    /// Encrypts and decrypts any data using chacha20 given a 12 byte nonce.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// # use fed_tickets::Context;
+    /// # async {
+    /// # let context = Context::new(None).await.unwrap();
+    /// let nonce: [u8; 12] = rand::random();
+    /// let data = b"secret data";
+    /// let encrypted = context.endecrypt(data, &nonce);
+    /// let decrypted = context.endecrypt(&encrypted, &nonce);
+    /// assert_eq!(decrypted, b"secret data");
+    /// # };
+    /// ```
+    #[must_use]
+    pub fn endecrypt(&self, data: &[u8], nonce: &[u8; 12]) -> Vec<u8> {
+        let mut cipher = ChaCha20::new(&self.encryption_key.into(), nonce.into());
+        let mut buffer = Vec::with_capacity(data.len() + 32);
+        buffer.extend_from_slice(data);
+        cipher.apply_keystream(&mut buffer);
+        buffer
+    }
+    /// Encrypts and decrypts a mutable byte slice using chacha20 given a 12 byte nonce.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// # use fed_tickets::Context;
+    /// # async {
+    /// # let context = Context::new(None).await.unwrap();
+    /// let nonce: [u8; 12] = rand::random();
+    /// let mut data = Vec::from(b"secret data");
+    /// context.endecrypt_mut_slice(&mut data, &nonce);
+    /// context.endecrypt_mut_slice(&mut data, &nonce);
+    /// assert_eq!(data, b"secret data");
+    /// # };
+    /// ```
+    pub fn endecrypt_mut_slice(&self, data: &mut [u8], nonce: &[u8; 12]) {
+        let mut cipher = ChaCha20::new(&self.encryption_key.into(), nonce.into());
+        cipher.apply_keystream(data);
+    }
+    /// Decrypts to a [`String`] using chacha20 given a 12 byte nonce.
+    ///
+    /// # Errors
+    ///
+    /// If the data was not UTF-8, this returns an error.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// # use fed_tickets::Context;
+    /// # async {
+    /// # let context = Context::new(None).await.unwrap();
+    /// let nonce: [u8; 12] = rand::random();
+    /// let data = b"secret data";
+    /// let encrypted = context.endecrypt(data, &nonce);
+    /// let decrypted = context.decrypt_string(&encrypted, &nonce).unwrap();
+    /// assert_eq!(decrypted, "secret data");
+    /// # };
+    /// ```
+    pub fn decrypt_string(
+        &self,
+        encrypted_data: &[u8],
+        nonce: &[u8; 12],
+    ) -> Result<String, std::string::FromUtf8Error> {
+        let vec = self.endecrypt(encrypted_data, nonce);
+        String::from_utf8(vec)
     }
 }
