a98ba00eb17cca7c4b28459072f9db15b741ef23
Icelk <main@icelk.dev>
2026-05-19 22:45:53 +0200

    [API] (Update): Add indexes to speed up activity requests

---
+++ fed-tickets/schema/40-indexes.sql
@@ -0,0 +1,12 @@
+-- this file is written in chronological order. Please look for indexes which do the thing you want to do before adding more
+
+-- when writing the activity details & activity list APIs
+create index group_members_by_member on group_members using hash (member_id);
+create index group_tree on groups using gist (admin_path);
+create index group_tree_eq on groups using hash (admin_path);
+create index user_group_settings_by_user on user_group_settings using hash (user_id);
+create index user_group_settings_by_group on user_group_settings using hash (group_id);
+create index activity_time_start on activities using btree (time_start);
+create index activity_time_end on activities using btree (time_end);
+create index activity_hosts_by_activity on activity_hosts using hash (activity_id);
+create index ticket_kind_by_activity on ticket_kinds using hash (activity_id);
