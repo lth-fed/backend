020c414346b76fc83d59510f5446e0ee89ea4255
Icelk <main@icelk.dev>
2026-08-27 22:33:01 +0200

    [INFRA] (Update): Push to dockerhub also

--- build-push.sh
+++ build-push.sh
@@ -17,4 +17,14 @@ podman login registry.esek.se
 ./build-images.sh
 # also update env in `./build-images.sh`
 POSTGRES_PASSWORD=unused GRAFANA_ADMIN_PASSWORD=unused RUSTFS_ACCESS_KEY=unused RUSTFS_SECRET_KEY=unused \
-    podman compose -f compose.prod.yaml push fed-auth transactions minilith
+podman compose -f compose.prod.yaml push fed-auth transactions minilith
+
+# Mirror the release images to Docker Hub. `docker.io` is the registry
+# endpoint used for repositories hosted at hub.docker.com.
+podman login docker.io
+for service in fed-auth transactions minilith; do
+    podman tag \
+        "${CONTAINER_REGISTRY}/${service}:${CONTAINER_TAG}" \
+        "docker.io/esekmacapar/${service}:${CONTAINER_TAG}"
+    podman push "docker.io/esekmacapar/${service}:${CONTAINER_TAG}"
+done
